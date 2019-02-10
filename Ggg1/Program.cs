59bc80edabcd019b27b9823f359a423c1fa8db80
Даня Awesome <zubow.dany@gmail.com>
2019-02-10 12:09:56 +0200

    Добавьте файлы проекта.

---
+++ Ggg1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ggg1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! G1 ");
+        }
+    }
+}
